b63dec8fe18d2f10b14c6dd27ef16e6f686b919e
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-06-03 16:36:41 +0500

    add this

---
+++ class_01/app.js
@@ -0,0 +1,2 @@
+console.log("hello world"); //maybe the double or single dot
+console.log('hello world'); // semicolon will be added in the js but in the ts we have no need of the semicolon compiler will automatically added semicolon to the javascript file 
